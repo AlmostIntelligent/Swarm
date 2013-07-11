653bb093b9ea57eab4fceee4e4a9fd9954577ca7
Christian Kulpa <chris@almostintelligent.de>
2013-07-11 08:28:24 +0200

    Added missing class files for example web app

---
+++ subprojects/server/src/main/java/org/gethydrated/swarm/mapping/ScannerService.java
@@ -0,0 +1,51 @@
+package org.gethydrated.swarm.mapping;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ *
+ */
+public class ScannerService extends TimerTask {
+
+    private Timer timer = new Timer();
+    private ServerService serverService;
+    private MappingService mappingService;
+
+    public void init() throws Exception {
+
+    }
+
+    public void start() throws Exception {
+        timer.scheduleAtFixedRate(this, 0, 60000);
+    }
+
+    public void stop() throws Exception {
+        timer.cancel();
+    }
+
+    public void destroy() throws  Exception {
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("scanning");
+    }
+
+    public void setServerService(org.gethydrated.swarm.server.ServerService serverService) {
+        this.serverService = serverService;
+    }
+
+    public ServerService getServerService() {
+        return serverService;
+    }
+
+    public void setMappingService(org.gethydrated.swarm.mapping.MappingService mappingService) {
+        this.mappingService = mappingService;
+    }
+
+    public MappingService getMappingService() {
+        return mappingService;
+    }
+}
